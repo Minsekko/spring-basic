55b110da06513142bf7a20db327b5a7346492b05
kms <komi254@naver.com>
2025-03-14 10:49:53 +0900

    feat : movieRepository 초기화

--- src/main/java/org/innovibe/spring/repository/MovieRepository.java
+++ src/main/java/org/innovibe/spring/repository/MovieRepository.java
@@ -16,22 +16,7 @@ public class MovieRepository {
     private DataSource dataSource;
     public boolean create(Movie movie) {
         boolean result = false;
-        try {
 
-            Connection conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement("insert into movies values(null,?,?,?,?)");
-            ps.setString(1, movie.getTitle());
-            ps.setString(2, movie.getDescription());
-            ps.setString(3, movie.getGenre());
-            ps.setDate(4,Date.valueOf(movie.getReleaseDate()));
-
-            ps.executeUpdate();
-            result = true;
-
-            conn.close();
-        } catch (SQLException e) {
-            System.out.println("MovieRepository.Create error");
-        }
 
         return result;
     }
